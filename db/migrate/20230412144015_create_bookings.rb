aeb4a468bcc3780a76dfba35a234b0675d1e75e1
jessica coueron <jcoueron@yahoo.fr>
2023-04-12 16:45:00 +0200

    models added

---
+++ db/migrate/20230412144015_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :car, null: false, foreign_key: true
+      t.date :starts_at
+      t.date :ends_at
+      t.boolean :accepted
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Booking.destroy_all
+Car.destroy_all
+User.destroy_all
+#users
+user1 = User.create!(
+  email:"jessica@lewagon.fr",
+  password:"lewagon"
+)
+
+user2 = User.create!(
+  email:"gerard@lewagon.fr",
+  password:"lewagon1"
+)
+
+user3 = User.create!(
+  email:"malika@lewagon.fr",
+  password:"lewagon3"
+)
+
+user4 = User.create!(
+  email:"sam@lewagon.fr",
+  password:"lewagon4"
+)
+#cars
+Car1 = Car.create!(
+  brand:"Fiat",
+  model:"500",
+  year_of_production:2007,
+  price_per_day:60,
+  user:user1
+
+)
+
+Car2 = Car.create!(
+  brand:"Renault",
+  model:"Twingo",
+  year_of_production:2003,
+  price_per_day:40,
+  user:user2
+
+)
+
+Car3 = Car.create!(
+  brand:"Volkswagen",
+  model:"Polo",
+  year_of_production:2013,
+  price_per_day:60,
+  user:user3
+
+)
+
+Car4 = Car.create!(
+  brand:"Audi",
+  model:"S5",
+  year_of_production:2020,
+  price_per_day:80,
+  user:user4
+
+)
